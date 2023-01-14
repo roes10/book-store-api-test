703eb6c6f3cf4c76f1afce6855c1a5bb80b1d13f
Ramon Oestreich <ramon.oestreich@gmail.com>
2023-01-14 19:02:41 -0300

    migration book store

--- database/migrations/2023_01_14_124412_create_book_stores_table.php
+++ database/migrations/2023_01_14_124412_create_book_stores_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('isbn')->nullable();
-            $table->decimal('value', 8, 2)->nullable();
+            $table->unsignedDecimal('value')->nullable();
             $table->timestamps();
         });
     }
