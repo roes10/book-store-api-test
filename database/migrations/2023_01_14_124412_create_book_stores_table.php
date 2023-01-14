ede820d79a9b61c97c0b2299d1b382c9c6ba21ad
Ramon Oestreich <ramon.oestreich@gmail.com>
2023-01-14 09:57:15 -0300

    bookstore model and migration

---
+++ database/migrations/2023_01_14_124412_create_book_stores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('isbn')->nullable();
+            $table->decimal('value', 8, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_stores');
+    }
+};
